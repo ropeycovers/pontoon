8f9aae008e36357a155949b7cd5166b982dbce29
ropeycovers <87244061+ropeycovers@users.noreply.github.com>
2023-03-05 20:54:14 +0000

    Pass eslint rules

--- pontoon.js
+++ pontoon.js
@@ -2,7 +2,7 @@
   'use strict'
   const print = s => process.stdout.write(s + '\n')
   const deck = () => {
-    return function f(n, a) { return n===0 ? a : [...f(n-1,a),n] }(52,[])
+    return (function f (n, a) { return n === 0 ? a : [...f(n - 1, a), n] })(52, [])
   }
   process.stdout.encoding = 'utf8'
   print('Pontoon')
